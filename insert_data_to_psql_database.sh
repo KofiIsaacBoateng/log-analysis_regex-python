e9708b6971b7f36a2bd9b89ae8884d274c44337f
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2023-09-11 21:44:56 +0000

    This script currently does nothing. However, it will be reading the csv files and insert them into a postgreSQL database.

--- insert_data_to_psql_database.sh
+++ insert_data_to_psql_database.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 #This script is going to read the data from both csv files and upload them unto a psql database
+
+echo This script is going to upload both csv files: error_report.csv and user_report.csv to a psql database.
